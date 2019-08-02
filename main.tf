ecf063dd91f9d69b696268d6cea43fdbc748a5eb
Matteo Castellini <matteo.castellini@buildo.io>
2019-08-02 11:29:00 +0200

    Fix formatting

--- main.tf
+++ main.tf
@@ -124,16 +124,16 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   custom_error_response {
-    error_code = 403
-    response_code = 200
-    response_page_path = "/index.html"
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
     error_caching_min_ttl = 300
   }
 
   custom_error_response {
-    error_code = 404
-    response_code = 200
-    response_page_path = "/index.html"
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
     error_caching_min_ttl = 300
   }
 }
