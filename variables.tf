64aa5199b985905e5a50aae52673a5007f4f9198
Benedetto <bennesp@users.noreply.github.com>
2021-04-22 14:15:32 +0200

    upgrade to terraform >=0.13

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "domain" {
-  type        = "string"
+  type        = string
   description = "The domain where to host the site. This must be the naked domain, e.g. `example.com`"
 }
 
 variable "enable_health_check" {
-  type        = "string"
+  type        = string
   default     = false
   description = "If true, it creates a Route53 health check that monitors the www endpoint and an alarm that triggers whenever it's not reachable. Please note this comes at an extra monthly cost on your AWS account"
 }
 
 variable "health_check_alarm_sns_topics" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "A list of SNS topics to notify whenever the health check fails or comes back to normal"
 }
 
 variable "enable_gzip" {
-  type        = "string"
+  type        = string
   default     = true
   description = "Whether to make CloudFront automatically compress content for web requests that include `Accept-Encoding: gzip` in the request header"
 }
